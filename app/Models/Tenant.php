e80fb70802e5a19f14864f1013dfe8dd9a0a05c2
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2025-05-07 02:25:38 +0630

    feat(tenant): add tenant creation command and update related files

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -10,4 +10,25 @@ use Stancl\Tenancy\Database\Concerns\HasDomains;
 class Tenant extends BaseTenant implements TenantWithDatabase
 {
     use HasDatabase, HasDomains;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'active', 
+        'data',
+    ];
+    
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'active' => 'boolean',
+        'data' => 'array',
+    ];
 }
